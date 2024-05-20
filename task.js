a3bbf6ea86e08f852dfd1791d1c17d553f780ffa
RuslanGon <ruslan441006@gmail.com>
2024-05-20 18:31:38 +0300

    wer

--- task.js
+++ task.js
@@ -18,19 +18,6 @@ button.addEventListener('click', () => {
 
 // <!-- 2 task -->
 
-// const link = document.querySelectorAll('.link')
-// const item = document.querySelectorAll('.item')
-// const list1 = document.querySelectorAll('.list1')
-
-// document.addEventListener('click', () => {
-// if(item.classList.contains('ower') == true) {
-//     list1.classList.remove('ower')
-// } else {
-//     list1.classList.add('ower')
-// }
-// })
-
-
 document.querySelectorAll('.item').forEach(item => {
         item.addEventListener('mouseenter', () => {
             const submenu = item.querySelector('.list1');
@@ -46,3 +33,5 @@ document.querySelectorAll('.item').forEach(item => {
             }
         });
     });
+
+    // <!-- 2 task -->
