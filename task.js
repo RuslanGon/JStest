a8e1c462396b67161d94257c52a0e9f2d94c91e5
RuslanGon <ruslan441006@gmail.com>
2024-05-20 18:26:34 +0300

    wer

--- task.js
+++ task.js
@@ -1,9 +1,9 @@
 // <!-- 1 task -->
 
 const button = document.querySelector('.btn')
-console.log(button);
+
 const menu = document.querySelector('.second-div')
-console.log(menu);
+
 
 button.addEventListener('click', () => {
     if(menu.classList.contains('disp') == true) {
@@ -14,3 +14,35 @@ button.addEventListener('click', () => {
 })
 
 // <!-- 1 task -->
+
+
+// <!-- 2 task -->
+
+// const link = document.querySelectorAll('.link')
+// const item = document.querySelectorAll('.item')
+// const list1 = document.querySelectorAll('.list1')
+
+// document.addEventListener('click', () => {
+// if(item.classList.contains('ower') == true) {
+//     list1.classList.remove('ower')
+// } else {
+//     list1.classList.add('ower')
+// }
+// })
+
+
+document.querySelectorAll('.item').forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            const submenu = item.querySelector('.list1');
+            if (submenu) {
+                submenu.classList.add('show');
+            }
+        });
+
+        item.addEventListener('mouseleave', () => {
+            const submenu = item.querySelector('.list1');
+            if (submenu) {
+                submenu.classList.remove('show');
+            }
+        });
+    });
